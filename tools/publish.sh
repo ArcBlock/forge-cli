b6a110f16464f114648b32499aef1488d8912540
wangshijun <wangshijun2010@gmail.com>
2019-06-05 14:55:04 +0800

    fix: npm publish

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,3 +1,12 @@
+git config --local user.name "wangshijun"
+git config --local user.email "wangshijun2010@gmail.com"
+
+git remote remove origin
+git remote add origin "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git remote -v
+git pull origin master
+git branch -a
+
 DEBUG=* node tools/setup-ci.js
 
 npm publish

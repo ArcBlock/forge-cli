7d783937a5ff8e5af5d64f43eab6f5f388f5069d
wangshijun <wangshijun2010@gmail.com>
2019-06-05 13:47:31 +0800

    chore: update readme and related script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,9 +1,5 @@
-git checkout master
-
 # publish
-VERSION=$(cat version | awk '{$1=$1;print}')
-lerna run build
-lerna publish $VERSION --yes
+npm publish
 
 # update readme
 node tools/update-readme.js

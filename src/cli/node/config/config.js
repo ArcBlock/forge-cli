dfbc7e362838b8e7a160eade68c0810c1ac86f16
wangshijun <wangshijun2010@gmail.com>
2019-07-09 18:54:31 +0800

    chore: prepare for config set

--- src/cli/node/config/config.js
+++ src/cli/node/config/config.js
@@ -4,20 +4,26 @@ const GraphQLClient = require('@arcblock/graphql-client');
 const { symbols, hr } = require('core/ui');
 const { config, webUrl } = require('core/env');
 
-async function main({ opts: { peer } }) {
-  if (peer) {
-    const client = new GraphQLClient(`${webUrl()}/api`);
-    // eslint-disable-next-line no-shadow
-    const { config } = await client.getConfig();
-    shell.echo(`${symbols.success} config for peer:`);
-    shell.echo(hr);
-    shell.echo(config);
-  } else {
-    const { forgeConfigPath } = config.get('cli');
-    shell.echo(hr);
-    shell.echo(`${symbols.info} config file path: ${forgeConfigPath}`);
-    shell.echo(hr);
-    shell.echo(fs.readFileSync(forgeConfigPath).toString());
+async function main({ args: [action = 'get'], opts: { peer } }) {
+  if (action === 'get') {
+    if (peer) {
+      const client = new GraphQLClient(`${webUrl()}/api`);
+      // eslint-disable-next-line no-shadow
+      const { config } = await client.getConfig();
+      shell.echo(`${symbols.success} config for peer:`);
+      shell.echo(hr);
+      shell.echo(config);
+    } else {
+      const { forgeConfigPath } = config.get('cli');
+      shell.echo(hr);
+      shell.echo(`${symbols.info} config file path: ${forgeConfigPath}`);
+      shell.echo(hr);
+      shell.echo(fs.readFileSync(forgeConfigPath).toString());
+    }
+  }
+
+  if (action === 'set') {
+    // FIXME: implement config set feature
   }
 }
 

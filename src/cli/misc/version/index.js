05cc91e12608863e7debe17248a24e81217a36b3
wangshijun <wangshijun2010@gmail.com>
2019-07-10 16:12:24 +0800

    fix: sort commands by alphabetic in the list #83

--- src/cli/misc/version/index.js
+++ src/cli/misc/version/index.js
@@ -2,16 +2,11 @@
 const { cli, action } = require('core/cli');
 const { execute, run } = require('./version');
 
-cli(
-  'version',
-  'Output version for all components, including forge-cli, forge, storage and consensus engine',
-  input => action(execute, run, input),
-  {
-    requirements: {
-      forgeRelease: true,
-      rpcClient: true,
-      wallet: false,
-    },
-    options: [],
-  }
-);
+cli('version', 'Output version for all forge components', input => action(execute, run, input), {
+  requirements: {
+    forgeRelease: true,
+    rpcClient: true,
+    wallet: false,
+  },
+  options: [],
+});

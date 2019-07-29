daae69711da9c5baf905424527bc995d9349cfcf
wangshijun <wangshijun2010@gmail.com>
2019-07-30 06:44:02 +0800

    feat: support install in silent mode #118

--- src/cli/node/init/index.js
+++ src/cli/node/init/index.js
@@ -13,17 +13,23 @@ cli(
       forgeRelease: false,
       rpcClient: false,
     },
-    options: [['-m, --mirror <url>', 'Mirror host used to download forge release']],
+    options: [
+      ['-m, --mirror <url>', 'Mirror host used to download forge release'],
+      ['-s, --silent', 'Install release silently, do not prompt for config customization'],
+    ],
     handlers: {
       '--help': () => {
         shell.echo(`
 Examples:
-  - ${chalk.cyan('forge install')}             download and activate latest version
-  - ${chalk.cyan('forge install 0.22.0')}      download and activate forge v0.22.0
-  - ${chalk.cyan('forge install v0.22.0')}     download and activate forge v0.22.0
+  - ${chalk.cyan(
+    'forge install'
+  )}             Download and activate latest version, prompt to customize config
+  - ${chalk.cyan('forge install --silent')}    Download and setup with default config
+  - ${chalk.cyan('forge install 0.22.0')}      Download and activate forge v0.22.0
+  - ${chalk.cyan('forge install v0.22.0')}     Download and activate forge v0.22.0
   - ${chalk.cyan(
     'forge install --mirror http://arcblock.oss-cn-beijing.aliyuncs.com'
-  )}      specify a custom mirror for download
+  )}      Download from custom mirror
         `);
       },
     },

38f3e7b335e036bb0ec63587f064387bb5cf4204
wangshijun <wangshijun2010@gmail.com>
2019-08-15 09:46:50 +0800

    chore: rename list => ls

--- src/cli/protocol/list/index.js
+++ src/cli/protocol/list/index.js
@@ -2,7 +2,7 @@
 const { cli, action } = require('core/cli');
 const { execute, run } = require('./list');
 
-cli('protocol:list', 'List transaction protocols', input => action(execute, run, input), {
+cli('protocol:ls', 'List transaction protocols', input => action(execute, run, input), {
   requirements: {
     forgeRelease: true,
     runningNode: true,

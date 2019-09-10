4abf029a307109c896a462a8fd5f0d6ca5effa8a
polunzh <zhenqiang@arcblock.io>
2019-09-10 10:10:18 +0800

    blocklet: support copy multiple files/dirs

--- src/cli/blocklet/lib/handlers/starter.js
+++ src/cli/blocklet/lib/handlers/starter.js
@@ -4,17 +4,13 @@ const path = require('path');
 const { isEmptyDirectory } = require('core/forge-fs');
 const { printInfo } = require('core/util');
 
-const verify = (blockletConfig, { cwd }) => {
+const verify = blockletConfig => {
   if (!isEmptyDirectory(process.cwd())) {
     throw new Error('Current directory is not empty');
   }
 
   if (!blockletConfig) {
     throw new Error('There is no sources in blocklet.json');
-  }
-
-  if (!fsExtra.existsSync(path.join(cwd, blockletConfig.sources))) {
-    throw new Error('Sources path in blocklet.json is not exists');
   }
 
   return true;
@@ -37,13 +33,32 @@ const runGenerateScripts = (scripts = {}, { cwd, targetDir }) => {
   }
 };
 
+/**
+ *
+ * @param {string|array} source
+ * @param {string} target
+ */
+const copyFiles = (sourceRoot, file, target) => {
+  let files = file;
+  if (!Array.isArray(file)) {
+    files = [file];
+  }
+
+  files.forEach(f => fsExtra.copySync(path.join(sourceRoot, f), target));
+};
+
 const run = async (blockletConfig, { cwd }) => {
   const { 'generate-scripts': scripts, hooks = {} } = blockletConfig;
   if (!hooks) {
     printInfo('No hooks');
     return;
   }
-  fsExtra.copySync(path.join(cwd, blockletConfig.sources), process.cwd());
+
+  if (hooks['pre-copy']) {
+    childProcess.execSync(hooks['pre-copy'], { stdio: 'inherit', cwd: process.cwd() });
+  }
+
+  copyFiles(cwd, blockletConfig.sources, process.cwd());
 
   runGenerateScripts(scripts, { cwd, targetDir: process.cwd() });
 

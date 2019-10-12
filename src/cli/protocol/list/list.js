c0b6caff5d7c7c9df3dfa3cebca10c900a7694fa
polunzh <zhenqiang@arcblock.io>
2019-10-12 17:54:47 +0800

    fix: forge chain:ls not list all protocols

--- src/cli/protocol/list/list.js
+++ src/cli/protocol/list/list.js
@@ -1,12 +1,11 @@
-const shell = require('shelljs');
 const Table = require('cli-table-redemption');
 const { messages } = require('@arcblock/forge-proto');
 const { createRpcClient } = require('core/env');
-const { printError, printWarning } = require('core/util');
+const { print, printError, printWarning } = require('core/util');
 
-const ensureProtocols = async (client, op) => {
-  const { state } = await client.getForgeState();
+const fetchProtocols = async client => {
   try {
+    const { state } = await client.getForgeState();
     const protocolsRaw = await Promise.all(
       state.protocolsList.map(
         ({ address }) =>
@@ -17,16 +16,14 @@ const ensureProtocols = async (client, op) => {
           })
       )
     );
-    const protocols = protocolsRaw.map(x => ({
-      name: x.itx.name,
-      address: x.itx.address,
-      version: x.itx.version,
-      status: x.status,
-    }));
 
-    // Get protocols that are disabled
-    const choices = protocols
-      .filter(x => (op === 'activate_protocol' ? x.status : x.status === 0))
+    const protocols = protocolsRaw
+      .map(x => ({
+        name: x.itx.name,
+        address: x.itx.address,
+        version: x.itx.version,
+        status: x.status,
+      }))
       .sort((a, b) => {
         if (a.name > b.name) {
           return 1;
@@ -38,16 +35,24 @@ const ensureProtocols = async (client, op) => {
         return 0;
       });
 
-    return choices;
+    return protocols;
   } catch (err) {
     printError('Failed to fetch protocol list', err);
     return [];
   }
 };
 
+const ensureProtocols = async (client, op) => {
+  const protocols = await fetchProtocols(client);
+  // Get protocols that are disabled
+  const choices = protocols.filter(x => (op === 'activate_protocol' ? x.status : x.status === 0));
+
+  return choices;
+};
+
 async function main() {
   const client = createRpcClient();
-  const protocols = await ensureProtocols(client);
+  const protocols = await fetchProtocols(client);
 
   // Fast return if all protocols are running
   if (!protocols.length) {
@@ -71,7 +76,7 @@ async function main() {
     ]);
   });
 
-  shell.echo(table.toString());
+  print(table.toString());
 }
 
 exports.run = main;

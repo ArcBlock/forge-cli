6fd5e37d6d303c5faef71242f4e33d5937b3016a
polunzh <zhenqiang@arcblock.io>
2019-11-08 17:14:48 +0800

    chore: revert print log post install

--- src/scripts/post-install.js
+++ src/scripts/post-install.js
@@ -1,10 +1,12 @@
 const chalk = require('chalk');
 const emoji = require('node-emoji');
 
-const { print } = require('../core/util');
+const { print, printLogo } = require('../core/util');
 
+printLogo();
 print(`${emoji.get('tada')} Congratulations! Forge CLI installed successfully\n`);
 print('It only takes 3 steps to create a new chain:');
 print(`1. ${chalk.cyan('forge install')}`);
 print(`2. ${chalk.cyan('forge chain:create')}`);
 print(`3. ${chalk.cyan('forge start')}`);
+print();

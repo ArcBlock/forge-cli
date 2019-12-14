0ea15b19609cd8c8230e8ded18a8ec1ace4b2815
wangshijun <wangshijun2010@gmail.com>
2019-12-14 10:16:37 +0800

    feat: support validator declare option

--- src/cli/node/declare/index.js
+++ src/cli/node/declare/index.js
@@ -18,11 +18,15 @@ cli(
       currentChainRunning: true,
       chainExists: true,
     },
+    options: [
+      ['--validator', 'Declare validator account on chain'],
+    ],
     handlers: {
       '--help': () => {
         print(`
 Examples:
 - ${chalk.cyan('forge declare:node')}                   Declare current node on chain
+- ${chalk.cyan('forge declare:node --validator')}                   Declare current node on chain
       `);
       },
     },

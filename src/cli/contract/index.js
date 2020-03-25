45cc1e960d398755a3d608c6930989bb01354515
wangshijun <wangshijun2010@gmail.com>
2020-03-26 06:30:35 +0800

    chore: cleanup transaction protocol related code

--- src/cli/contract/index.js
+++
@@ -1,6 +0,0 @@
-exports.activate = require('./activate');
-exports.compile = require('./compile');
-exports.create = require('./create');
-exports.deactivate = require('./deactivate');
-exports.deploy = require('./deploy');
-exports.list = require('./list');

d203c704e7eaf641475dda9ea5822df8a91f34bc
wangshijun <wangshijun2010@gmail.com>
2020-03-26 08:53:09 +0800

    chore: cleanup stake related feature

--- src/cli/tx/index.js
+++ src/cli/tx/index.js
@@ -2,5 +2,3 @@ exports.display = require('./display');
 exports.list = require('./list');
 // exports.send = require('./send');
 // exports.sign = require('./sign');
-// exports.stake = require('./stake');
-// exports.unstake = require('./unstake');
